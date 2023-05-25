00f03149575d97d2d425d21be640c8497e030ed4
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-05-25 00:14:44 -0300

    Adicionar arquivos de projeto.

---
+++ mvc-crud-dotnet-postgres/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_crud_dotnet_postgres.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
